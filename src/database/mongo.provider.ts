6e5494a5d0868bf859ceec8068d62ebd819d9853
mohsin ali <xyster000@gmail.com>
2026-01-10 03:42:28 +0500

    db add

--- src/database/mongo.provider.ts
+++ src/database/mongo.provider.ts
@@ -3,7 +3,9 @@ import { MongoClient, Db } from 'mongodb';
 export const MongoProvider = {
   provide: 'MONGO_DB',
   useFactory: async (): Promise<Db> => {
-    const client = new MongoClient('mongodb://localhost:27017');
+    //mongodb://localhost:27017
+
+    const client = new MongoClient('mongodb+srv://xyster:lVDBP8OSfcr7uXVg@cluster0.avqldj6.mongodb.net/');
     await client.connect();
     return client.db('currency_db');
   },
