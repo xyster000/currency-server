31d55d6e31915ace43eac3abbbff7ebab594bcd6
mohsin ali <xyster000@gmail.com>
2026-01-10 04:34:56 +0500

    cors

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,7 @@ async function bootstrap() {
     process.env.CURRENCY_API_KEY,
   );
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [
-      'http://localhost:4200',
-      'https://currencyconverter1011.netlify.app',
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
